5626532e1185b572dbe61c35b68765a8e67fd008
Dan Palmieri <dlspalmieri@gmail.com>
2022-02-02 10:14:45 -0300

    fix

--- resources/views/components/pricing-table.blade.php
+++ resources/views/components/pricing-table.blade.php
@@ -1,4 +1,3 @@
-<div
-    class="space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
+<div {{ $attributes->merge(['class' => 'space-y-4 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4']) }}>
     {{ $slot }}
 </div>
