0ef9c04398535574bb3c07bb16a9635d92f69871
Dan Palmieri <dlspalmieri@gmail.com>
2022-02-02 10:31:16 -0300

    fi

--- resources/views/components/pricing-table.blade.php
+++ resources/views/components/pricing-table.blade.php
@@ -1,3 +1,17 @@
-<div {{ $attributes->merge(['class' => 'space-y-4 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4']) }}>
-    {{ $slot }}
+@props([
+    'hasAnual' => false,
+    'anualDiscount' => 20
+])
+<div x-data="{'show': 'monthly'}">
+    @if($hasAnual)
+    <div class="flex justify-center">
+        <div class="relative self-center mb-8 bg-gray-200 rounded-lg p-0.5 flex sm:mt-8">
+            <button @click="show='monthly'" :class="show==='monthly' ? 'bg-white text-gray-900 shadow-sm' : 'text-gray-700 border-transparent'" type="button" class="relative w-1/2 border-gray-200 rounded-md  py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:z-10 sm:w-auto sm:px-8">Mensal</button>
+            <button @click="show='anualy'" :class="show==='anualy' ? 'bg-white text-gray-900 shadow-sm' : 'text-gray-700 border-transparent'" type="button" class="ml-0.5 relative w-1/2 border rounded-md py-2 text-sm font-medium  whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:z-10 sm:w-auto sm:px-8">Anual ({{ $anualDiscount }}% OFF)</button>
+          </div>
+    </div>
+    @endif
+    <div class="space-y-4 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
+        {{ $slot }}
+    </div>
 </div>
--- resources/views/components/pricing/plan.blade.php
+++ resources/views/components/pricing/plan.blade.php
@@ -1,19 +1,26 @@
 @props([
     'name',
     'description',
-    'price',
-    'period' => '/mês'
+    'priceMonthly',
+    'priceAnnualy'
 ])
 <div class="border border-gray-200 rounded-lg shadow-sm divide-y divide-gray-200">
     <div class="p-6">
         <h2 class="text-lg leading-6 font-medium text-gray-900">{{ $name }}</h2>
+        @if($description)
         <p class="mt-4 text-sm text-gray-500">{{ $description }}</p>
+        @endif
         <p class="mt-8">
-            <span class="text-4xl font-extrabold text-gray-900">{{ $price }}</span>
-            <span class="text-base font-medium text-gray-500">{{ $period }}</span>
+            <span class="text-4xl font-extrabold text-gray-900" x-text="show==='monthly' ? '{{ $priceMonthly }}' : '{{ $priceAnnually }}'"></span>
+            <span class="text-base font-medium text-gray-500" x-text="show==='monthly' ? '/mês' : '/ano'"></span>
         </p>
         <div class="mt-8">
-            {{ $cta ?? '' }}
+            <span x-show="'show'==='monthly'">
+                {{ $ctaMonthly ?? '' }}
+            </span>
+            <span x-show="'show'==='annually'">
+                {{ $ctaAnnually ?? '' }}
+            </span>
         </div>
     </div>
     <div class="pt-6 pb-8 px-6">
