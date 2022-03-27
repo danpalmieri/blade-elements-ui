aee01c006aea328c4fe0284be03367de123a815e
Daniel Palmieri <dlspalmieri@gmail.com>
2022-03-27 10:40:27 -0300

    fix

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -5,6 +5,8 @@
     'tag' => 'button',
     'type' => 'button',
     'size' => 'md',
+    'spinner' => false,
+    'spinnerIconSize' => 'w-5 h-5'
 ])
 
 @php
@@ -48,6 +50,20 @@
 
         @if ($icon && $iconPosition === 'after')
             <x-dynamic-component :component="$icon" :class="$iconClasses" />
+        @endif
+
+        @if ($spinner)
+        <svg class="animate-spin {{ $spinnerIconSize }} shrink-0"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            @if (preg_replace('/[^a-zA-Z]+/', '', $spinner))
+                wire:target="{{ $spinner }}"
+            @endif
+            wire:loading.delay{{ $loadingDelay ? ".{$loadingDelay}":'' }}>
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
         @endif
     </button>
 @elseif ($tag === 'a')
