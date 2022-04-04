2d0184c6a9976f7e67bf6335742bdb1dbdfd1f40
Daniel Palmieri <dlspalmieri@gmail.com>
2022-04-04 15:39:12 -0300

    fix

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -7,7 +7,7 @@
     <a {{ $attributes->class([
         'flex items-center h-10 px-2 space-x-2 rounded-lg',
         'transition hover:bg-gray-500/5 focus:bg-primary-500/10 focus:text-primary-600 focus:outline-none' => ! $active,
-        'text-white bg-primary-600' => $active,
+        'text-primary-600 bg-primary-200' => $active,
     ]) }}>
         @if ($icon)
             <x-dynamic-component :component="$icon" :class="'w-7 h-7' . ($active ? '' : ' text-primary-500')" />
